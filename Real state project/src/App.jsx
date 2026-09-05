74cb32cba0101dc10ad528264b030ab9189a40b2
Mohit Singh <mohitsinghmehta99@gmail.com>
2026-09-05 20:01:37 +0530

    first commit

---
+++ Real state project/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import About from './components/About'
+import Projects from './components/Projects'
+import Testimonails from './components/Testimoninal'
+import Contact from './components/contact'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Footer from './components/Footer'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    <div className='w-full overflow-hidden'>
+ <ToastContainer/>
+ <Header/>
+ <About/>
+ <Projects/>
+ <Testimonails/>
+ <Contact/>
+ <Footer/>
+    </div>
+  
+    </>
+  )
+}
+
+export default App
